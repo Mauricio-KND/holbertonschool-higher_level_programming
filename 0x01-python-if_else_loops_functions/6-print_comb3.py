b75a54bc2f6250ccfc37122be0b6e03bff8c6325
Mauricio-KND <1299@holbertonschool.com>
2019-12-10 21:05:38 +0000

    Task number 6.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for first in range(10):
+    for second in range(10):
+        if first is 8 and second is 9:
+            print('{:d}{:d}'.format(first, second))
+        elif first < second:
+                  print('{:d}{:d}'.format(first, second), end=', ')

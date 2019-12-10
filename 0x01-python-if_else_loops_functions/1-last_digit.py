a8e1ec42c9b90a1dd30da1c50b3189122fb049d8
Mauricio-KND <1299@holbertonschool.com>
2019-12-10 18:06:51 +0000

    Task number 1.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ print("Last digit of {:d} is {:d} and is".format(number, last), end=' ')
 if last > 5:
     print("greater than 5")
 elif last == 0:
-    print("is 0")
+    print("0")
 else:
     print("less than 6 and not 0")

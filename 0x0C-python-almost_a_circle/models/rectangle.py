8f14cdb0d7fd8c59cb937bdbb0c77f368e7fbd77
Mauricio-KND <1299@holbertonschool.com>
2020-01-27 18:38:50 +0000

    Commit from 0 to 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,6 @@ from models.base import Base
 class Rectangle(Base):
     """Rectangle Class inherits from Base."""
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializing the class."""
         super().__init__(id)
@@ -15,7 +14,7 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-        
+
     @property
     def width(self):
         """Get the widht."""

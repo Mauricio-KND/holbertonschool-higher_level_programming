f8601c6976c18fc2020cfc919ec201a3ab0f03b8
Mauricio-KND <1299@holbertonschool.com>
2020-01-27 20:12:27 +0000

    Commit of task number 10.

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defining Square Class."""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square Class inherits from Rectangle."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializing the class."""
+        super().__init__(size, size, x, y, id)

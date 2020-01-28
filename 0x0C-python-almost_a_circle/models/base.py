c06d01fcb26fcd062db331fa1ec1a66224b30aa9
Mauricio-KND <1299@holbertonschool.com>
2020-01-28 01:11:04 +0000

    Commit of task number 15.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defining Base Class."""
 
+import json
+
 
 class Base:
     """Base Class."""
@@ -14,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Converts a given list of dictionaries to a json string."""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)

1a6bf9cc07174a9ab8b4c96b9b697cb8c62a6602
Mauricio-KND <1299@holbertonschool.com>
2020-03-03 04:32:43 +0000

    Commit task 8.

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Inserts a new row in the table first_table.
--- Syntax: INSERT [LOW_PRIORITY | DELAYED | HIGH_PRIORITY] [IGNORE][INTO] tbl_name
+-- Syntax: INSERT [LOW_PRIORITY | DELAYED | HIGH_PRIORITY] [IGNORE][INTO] tbl_name.
 INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Displays the number of records with id = 89 in first_table of a database.
+-- Syntax: SELECT [FROM table_references] [WHERE where_condition].
+SELECT COUNT(*) FROM first_table WHERE id = 89;

df0ce40b6c55628e132920fd1f73da42f0df3664
Mauricio-KND <1299@holbertonschool.com>
2020-03-03 04:44:15 +0000

    Commit task 9.

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- Creates a table second_table in the database hbtn_0c_0 in a MySQL server and add multiples rows.
+-- Syntax: CREATE [TEMPORARY] TABLE [IF NOT EXISTS] tbl_name.
+-- INSERT [INTO] tbl_name. Inserts new rows into an existing table.
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);

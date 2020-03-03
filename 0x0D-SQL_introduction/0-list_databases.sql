bcaabd9d3bbd78a8ca9ba7f2e9b5e48118acceb3
Mauricio-KND <1299@holbertonschool.com>
2020-03-03 02:41:00 +0000

    Erasing sql files.

--- 0x0D-SQL_introduction/0-list_databases.sql
+++
@@ -1,3 +0,0 @@
---Lists all databases of a MySQL server.
---SHOW DATABASES statement, lists the databases on a MySQL server host.
-SHOW DATABASES;
--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++
@@ -1,3 +0,0 @@
---Lets see
---Again. Again
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_o`;

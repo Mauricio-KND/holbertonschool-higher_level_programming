24bdf346c19a8a75aa881413e7ce8db94b576a6c
Mauricio-KND <1299@holbertonschool.com>
2020-03-03 01:51:01 +0000

    Commit of task number 0. Not tested on MySQL.

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
 --Lists all databases of a MySQL server.
---SHOW DATABASES statement, lists databases on MySQL server host. Implements databases as directories.
+--SHOW DATABASES statement, lists databases on MySQL server host.
 SHOW DATABASES;

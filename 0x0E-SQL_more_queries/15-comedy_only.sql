2d5ab9561908fd4d7231b000fc7a15587afb600e
Mauricio-KND <1299@holbertonschool.com>
2020-03-04 03:17:33 +0000

    Commit of task number 15.

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,8 @@
--- Lists all Comedy shows in the database hbtn_0d_tvshows.
--- SELECT statement with clauses. 
-The tv_genres table contains only one record where name = Comedy
-SELECT tv_shows.title
-FROM tv_genres INNER JOIN tv_show_genres
-	ON (tv_genres.name = 'Comedy' and tv_show_genres.genre_id = tv_genres.id)
-	INNER JOIN tv_shows
-	ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title ASC;
+-- 
+-- 
+SELECT s.title FROM tv_genres g, tv_show_genres t, tv_shows s
+WHERE g.id = t.genre_id
+    AND t.show_id = s.id
+    AND g.name = "Comedy"
+ORDER BY s.title ASC;
+; 

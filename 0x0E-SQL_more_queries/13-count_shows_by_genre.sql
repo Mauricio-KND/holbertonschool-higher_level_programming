6e2b6af2ce5c85f55990ec43dd959a90d3551a05
Mauricio-KND <1299@holbertonschool.com>
2020-03-04 03:07:45 +0000

    Commit of task number 13.

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,6 @@
 -- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 -- SELECT statement retrieves rows selected from one or more tables.
 SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_shows'
--- FROM clause lists the tables and any joins in "tv_genres".
--- INNER JOIN keyword selects records that have matching values in both tables (tv_genres/tv_show_genres).
 FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
 ORDER BY number_shows DESC;

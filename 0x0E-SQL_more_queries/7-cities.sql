85703cd6b33dd8d60ea0a94f3d1dc3e6d07bb548
Mauricio-KND <1299@holbertonschool.com>
2020-03-04 01:16:31 +0000

    Commit of task number 11.

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,9 +4,10 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- CREATE DATABASE creates a database with the given name.
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     -- The UNIQUE constraint ensures that all values in a column are different.
-    -- Primary key for a table represents the column/set of columns used in your most vital queries.
+    -- Primary key for a table represents the column/set of columns used in most vital queries.
        id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
        state_id INT NOT NULL,
+       --  FOREIGN KEY Constraints permit cross-referencing data across tables, and foreign key constraints.
        FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states (id),
        name VARCHAR(256) NOT NULL
 );

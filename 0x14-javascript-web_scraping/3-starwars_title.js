3256e41d5b65346452e96991a7037fec9aa55078
Mauricio-KND <1299@holbertonschool.com>
2020-05-12 19:26:29 +0000

    Writing until 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, { json: true }, function (err, res, body) {
   if (err) { return console.log(err); }
   console.log(body.title);
